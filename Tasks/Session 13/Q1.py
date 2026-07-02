45e7639273b1faa9955b4e9a3bb4362497d97cf6
Vaibhav Kate <vaibhavkate1301@gmail.com>
2026-07-02 09:44:25 +0530

    Delete Tasks/Session 13/Q1.py

--- Tasks/Session 13/Q1.py
+++
@@ -1,7 +0,0 @@
-# Q1. Basic Array Creation)
-# Write a program to:
-# a) Create a 1D array with elements 10, 20, 30, 40, 50] using np.array().
-# b) Create a 2D array 33) using np.array().
-# Print both arrays along with their shape and data type (dtype)
-
-
